5047e023006b233efbca0a2beda6a741bcd09aeb
Shawn <140131192+LW-Shawn@users.noreply.github.com>
2024-12-31 21:17:40 +0800

    Update script.js
    //随机文字烟花内容

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 Copyright © 2022 NianBroken. All rights reserved.
 Github：https://github.com/NianBroken/Firework_Simulator
 Gitee：https://gitee.com/nianbroken/Firework_Simulator
-本项目采用 Apache-2.0 许可证
+本项目采用 Apache-2.0 许可证This project is licensed under the Apache-2.0 license
 简而言之，你可以自由使用、修改和分享本项目的代码，但前提是在其衍生作品中必须保留原始许可证和版权信息，并且必须以相同的许可证发布所有修改过的代码。
 */
 
@@ -78,7 +78,7 @@ const mainStage = new Stage("main-canvas");
 const stages = [trailsStage, mainStage];
 
 //随机文字烟花内容
-const randomWords = ["新年快乐", "心想事成"];
+const randomWords = ["新年快乐", "心想事成", "我爱你"];
 const wordDotsMap = {};
 randomWords.forEach((word) => {
 	wordDotsMap[word] = MyMath.literalLattice(word, 3, "Gabriola,华文琥珀", "90px");
